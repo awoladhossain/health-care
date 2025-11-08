928f48b39bfe87bff86b7d2464df9f27829d8190
awoladhossain <awoladh04@gmail.com>
2025-11-08 13:19:55 +0600

    updated the controller and service of admin

--- health-care-backend/src/app/modules/Admin/admin.controller.ts
+++ health-care-backend/src/app/modules/Admin/admin.controller.ts
@@ -1,12 +1,38 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import pick from "../../../shared/pick";
 import { AdminService } from "./admin.service";
+
 const getAllFromDB = async (req: Request, res: Response) => {
-  const result = await AdminService.getAllFromDB();
-  res.status(200).json({
-    status: 200,
-    message: "Admins Fetched Successfully!",
-    data: result,
-  });
+  try {
+    const safeQuery = pick(req.query, [
+      "name",
+      "email",
+      "searchTerm",
+      "contactNumber",
+    ]);
+
+    const result = await AdminService.getAllFromDB(safeQuery);
+    res.status(200).json({
+      status: 200,
+      message: "Admins Fetched Successfully!",
+      data: result,
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: err.name || "Failed to fetch Admins",
+        error: err.message,
+      });
+    }
+
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Failed to fetch Admins",
+      error: err,
+    });
+  }
 };
 
 export const AdminController = {
--- health-care-backend/src/app/modules/Admin/admin.service.ts
+++ health-care-backend/src/app/modules/Admin/admin.service.ts
@@ -1,12 +1,55 @@
-import { PrismaClient } from "@prisma/client"
+import { Prisma, PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const getAllFromDB = async ()=>{
-  const result = await prisma.admin.findMany();
+const getAllFromDB = async (params: any) => {
+
+  const { searchTerm, ...rest } = params;
+
+  const addCondition: Prisma.AdminWhereInput[] = [];
+  const adminSearchableFields: string[] = ["name", "email"];
+
+  // [
+  //       {
+  //         name: {
+  //           contains: params.searchTerm,
+  //           mode: "insensitive",
+  //         },
+  //       },
+  //       {
+  //         email: {
+  //           contains: params.searchTerm,
+  //           mode: "insensitive",
+  //         },
+  //       },
+  //     ],
+  if (params.searchTerm) {
+    addCondition.push({
+      OR: adminSearchableFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+  if (Object.keys(rest).length > 0) {
+    addCondition.push({
+      AND: Object.keys(rest).map((key) => ({
+        [key]: {
+          equals: rest[key],
+        },
+      })),
+    });
+  }
+
+  const whereConditions: Prisma.AdminWhereInput = { AND: addCondition };
+  const result = await prisma.admin.findMany({
+    where: whereConditions,
+  });
   return result;
-}
+};
 
 export const AdminService = {
-  getAllFromDB
-}
+  getAllFromDB,
+};
