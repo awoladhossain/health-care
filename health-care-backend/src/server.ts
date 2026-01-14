1c9c32e2f84e014c8a0b2ad308a56cc31bf4b7e3
awoladhossain <awoladh04@gmail.com>
2026-01-14 21:29:39 +0600

    added appointment schema

--- health-care-backend/src/server.ts
+++ health-care-backend/src/server.ts
@@ -1,15 +1,11 @@
-import dotenv from "dotenv";
 import { Server } from "http";
 import app from "./app";
 import config from "./config";
 
-
 const port = config.port || 5000;
-
 async function main() {
   const server: Server = app.listen(port, () => {
     console.log(`Server is running on Port: ${port}`);
   });
 }
-
 main();
