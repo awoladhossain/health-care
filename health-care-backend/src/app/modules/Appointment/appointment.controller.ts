86978d2be91c4ddf076f87ca3d315df4f9d3eff7
awoladhossain <awoladh04@gmail.com>
2026-01-15 23:36:59 +0600

    appointment module completed

--- health-care-backend/src/app/modules/Appointment/appointment.controller.ts
+++ health-care-backend/src/app/modules/Appointment/appointment.controller.ts
@@ -2,14 +2,17 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { sendResponse } from "../../../helpers/sendResponseHelper";
 import catchAsync from "../../../shared/catchAsync";
+import pick from "../../../shared/pick";
 import { IAuthUser } from "../../interfaces/common";
+import { appointmentFilterableFields } from "./appointment.constant";
 import { AppointmentService } from "./appointment.service";
 
 const createAppointment = catchAsync(
   async (req: Request & { user?: IAuthUser }, res: Response) => {
     const user = req.user;
     const result = await AppointmentService.createAppointment(
-      user as IAuthUser,req.body
+      user as IAuthUser,
+      req.body
     );
     sendResponse(res, {
       statusCode: StatusCodes.OK,
@@ -20,6 +23,60 @@ const createAppointment = catchAsync(
   }
 );
 
+const getMyAppointments = catchAsync(
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const user = req.user;
+    const filters = pick(req.query, ["status", "paymentStatus"]);
+    const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+
+    const result = await AppointmentService.getMyAppointment(
+      user as IAuthUser,
+      filters,
+      options
+    );
+    sendResponse(res, {
+      statusCode: StatusCodes.OK,
+      success: true,
+      message: "Appointments Fetched Successfully!",
+      data: result,
+    });
+  }
+);
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, appointmentFilterableFields);
+  const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+  const result = await AppointmentService.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "Appointment retrieval successfully",
+    meta: result.meta,
+    data: result.data,
+  });
+});
+const changeAppointmentStatus = catchAsync(
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const { id } = req.params;
+    const { status } = req.body;
+    const user = req.user;
+
+    const result = await AppointmentService.changeAppointmentStatus(
+      id!,
+      status,
+      user as IAuthUser
+    );
+    sendResponse(res, {
+      statusCode: StatusCodes.OK,
+      success: true,
+      message: "Appointment status changed successfully",
+      data: result,
+    });
+  }
+);
 export const AppointmentController = {
   createAppointment,
+  getMyAppointments,
+  getAllFromDB,
+  changeAppointmentStatus,
 };
--- health-care-backend/src/app/modules/Appointment/appointment.routes.ts
+++ health-care-backend/src/app/modules/Appointment/appointment.routes.ts
@@ -1,14 +1,33 @@
 import { UserRole } from "@prisma/client";
 import express from "express";
 import auth from "../../middlewares/auth";
+import validateRequest from "../../middlewares/validateRequest";
 import { AppointmentController } from "./appointment.controller";
+import { AppointmentValidation } from "./appointment.validation";
 
 const router = express.Router();
+
+router.get(
+  "/",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AppointmentController.getAllFromDB
+);
+router.get(
+  "/my-appointments",
+  auth(UserRole.PATIENT),
+  AppointmentController.getMyAppointments
+);
 
 router.post(
   "/",
   auth(UserRole.PATIENT),
+  validateRequest(AppointmentValidation.createAppointment),
   AppointmentController.createAppointment
 );
 
+router.patch(
+  "/status/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR),
+  AppointmentController.changeAppointmentStatus
+);
 export const AppointmentRoutes = router;
